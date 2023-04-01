f740e01bf5e51e482de152cd3b6d8070c256e9d1
Samuel Mothwa <samuel@flatrocksolutions.co.za>
2023-04-01 18:29:16 +0200

    Navbar reverse to deployed servers

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -49,10 +49,10 @@ export default function Navbar() {
         <div className="navbar-item">
           <div className="buttons">
             <button className="button is-outlined" onClick={() => {
-                router.push("https://payfona.netlify.app/login");
+                router.push("https://payfona.com/login");
             }} >Login</button>
             <button className="button is-primary" onClick={() => {
-                router.push("https://payfona.netlify.app/register");
+                router.push("https://payfona.com/register");
             }}>
               Sign up
             </button>
